0018737231f2ff44697fbdd2d7bda01be5904315
INHOUSE-8 <inhouse8@ednindo.net>
2025-03-16 14:32:10 +0700

    save post

--- api/post.php
+++ api/post.php
@@ -38,7 +38,16 @@ function post_save()
 
     $data = file_get_contents('php://input');
     $data = json_decode($data, true);
-    var_dump($data);
+    // var_dump($data);
+
+    $result = $db->query("INSERT INTO posts VALUES(null, '".$data['title']."', '".$data['content']."')");
+    if ($db->changes() > 0) {
+        header('Content-type: application/json');
+        echo json_encode([
+            'status' => 201,
+            'message' => 'Success add new post'
+        ]);
+    }
 
 }
 
