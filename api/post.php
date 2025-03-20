54bda37d0420fec64ab497e09fac9c63cde23bac
ryunze <ryunzeroeight08@gmail.com>
2025-03-21 01:18:14 +0800

    refactor file

--- api/post.php
+++ api/post.php
@@ -1,62 +1,33 @@
 <?php
 
+require_once(__DIR__ . '/../app/Conn.php');
+
+class PostAPI extends Conn {
+
+    function post_save()
+    {
+
+        $data = file_get_contents('php://input');
+        $data = json_decode($data, true);
+
+        $result = $this->db->query("INSERT INTO posts VALUES(null, '".$data['title']."', '".$data['content']."')");
+        if ($this->db->changes() > 0) {
+            header('Content-type: application/json');
+            echo json_encode([
+                'status' => 201,
+                'message' => 'Success add new post'
+            ]);
+        }
+
+    }
+}
+
+$postApi = new PostAPI();
+
 switch ($_SERVER['REQUEST_METHOD'])
 {
-    case 'GET':
-        post_get();
-        break;
     case 'POST':
-        post_save();
-        break;
-    case 'UPDATE':
-        post_update();
-        break;
-    case 'DELETE':
-        post_delete();
+        $postApi->post_save();
         break;
 }
 
-function post_get()
-{
-    $db = new SQLite3('../app/pombo.db');
-    
-    if (!$db) {
-        echo $db->lastErrorMsg();
-        exit;
-    }
-
-}
-
-function post_save()
-{
-    $db = new SQLite3('../app/pombo.db');
-    
-    if (!$db) {
-        echo $db->lastErrorMsg();
-        exit;
-    }
-
-    $data = file_get_contents('php://input');
-    $data = json_decode($data, true);
-    // var_dump($data);
-
-    $result = $db->query("INSERT INTO posts VALUES(null, '".$data['title']."', '".$data['content']."')");
-    if ($db->changes() > 0) {
-        header('Content-type: application/json');
-        echo json_encode([
-            'status' => 201,
-            'message' => 'Success add new post'
-        ]);
-    }
-
-}
-
-function post_update()
-{
-
-}
-
-function post_delete()
-{
-
-}
--- app/Conn.php
+++ app/Conn.php
@@ -6,14 +6,14 @@ class Conn {
 
     function __construct()
     {
-        // echo "CONN";
+        // Make SQLITE Connection
         $this->db = new SQLite3(__DIR__ . '/pombo.db');
     }
 
     public function get_all_post()
     {
         
-        $data = $this->db->query("SELECT * FROM posts");
+        $data = $this->db->query("SELECT * FROM posts ORDER BY id DESC");
         
         $rows = [];
         while ($row = $data->fetchArray(SQLITE3_ASSOC)) 
